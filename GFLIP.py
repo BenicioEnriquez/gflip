1d6e07d36ec4143d8579a46d75407f6b4125794d
Benicio Enriquez <mobenny99@gmail.com>
2024-07-23 14:46:32 -0400

    stroodles

--- GFLIP.py
+++ GFLIP.py
@@ -3,9 +3,9 @@ import torch.nn as nn
 import numpy as np
 import torch.nn.functional as F
 
-class GBlock(nn.Module):
+class XBlock(nn.Module):
     def __init__(self, ch, mid, f1, f2):
-        super(GBlock, self).__init__()
+        super(XBlock, self).__init__()
 
         self.cx = f2 != 0
 
@@ -13,12 +13,12 @@ class GBlock(nn.Module):
             nn.Conv2d(ch, ch, f1, 1, (f1-1)//2, 1, ch, False),
             nn.GroupNorm(1, ch),
             nn.Conv2d(ch, mid, 1, 1, 0, 1, 1, False),
-            nn.ReLU6(),
+            nn.ReLU(inplace=True),
         )
         if self.cx:
             self.mid = nn.Sequential(
                 nn.Conv2d(mid, mid, f2, 1, (f2-1)//2, 1, mid, False),
-                nn.ReLU6(),
+                nn.ReLU(inplace=True),
             )
         self.tail = nn.Conv2d(mid, ch, 1, 1, 0, 1, 1, False)
 
@@ -28,85 +28,98 @@ class GBlock(nn.Module):
             y = self.mid(y)
         return self.tail(y) + x
 
+class Genloss(nn.Module):
+    def __init__(self):
+        super(Genloss, self).__init__()
+
+        self.net = nn.Sequential(
+            nn.Conv2d(4, 64, 3, 1, 1),
+            XBlock(64, 196, 7, 5),
+            nn.Conv2d(64, 64, 3, 2, 1),
+            XBlock(64, 196, 7, 5),
+            nn.Conv2d(64, 64, 3, 2, 1),
+            XBlock(64, 196, 7, 5),
+            nn.Conv2d(64, 64, 3, 2, 1),
+            XBlock(64, 196, 7, 5),
+            nn.Conv2d(64, 64, 3, 2, 1),
+            XBlock(64, 196, 7, 5),
+            nn.Conv2d(64, 1, 3, 2, 1)
+        )
+    
+    def forward(self, x, depth):
+        x = torch.concat([x, depth], dim=1)
+        return self.net(x).mean(dim=[1,2,3])
+
+
 class Generator(nn.Module):
     def __init__(self):
         super(Generator, self).__init__()
 
         self.clipblk4 = nn.Sequential(
             nn.Upsample(scale_factor=4, mode='bilinear'),
+            nn.Conv2d(512, 512, 5, 1, 2),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 512, 5, 1, 2, bias=False),
-            nn.ReLU6(),
+            nn.ReLU(inplace=True),
             nn.Conv2d(512, 256, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.clipblk8 = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(256, 256, 3, 1, 1, bias=False),
-            nn.ReLU6(),
+            nn.Conv2d(256, 256, 3, 1, 1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(256, 128, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.clipblk16 = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(128, 128, 3, 1, 1, bias=False),
-            nn.ReLU6(),
+            nn.Conv2d(128, 128, 3, 1, 1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(128, 64, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.clipblk32 = nn.Sequential(
             nn.Upsample(scale_factor=2, mode='bilinear'),
-            nn.Conv2d(64, 64, 3, 1, 1, bias=False),
-            nn.ReLU6(),
+            nn.Conv2d(64, 64, 3, 1, 1),
+            nn.ReLU(inplace=True),
             nn.Conv2d(64, 32, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
 
-        self.imgblk32 = nn.Sequential(
-            nn.Conv2d(4, 32, 3, 1, 1),
-            nn.ReLU6()
-        )
-        self.imgblk16 = nn.Sequential(
-            nn.Conv2d(32, 64, 3, 2, 1, bias=False),
-            nn.ReLU6()
-        )
-        self.imgblk8 = nn.Sequential(
-            nn.Conv2d(64, 128, 3, 2, 1, bias=False),
-            nn.ReLU6()
-        )
-        self.imgblk4 = nn.Sequential(
-            nn.Conv2d(128, 256, 3, 2, 1, bias=False),
-            nn.ReLU6()
-        )
+        self.imgblk32 = nn.Conv2d(4, 32, 3, 1, 1)
+        self.imgblk16 = nn.Conv2d(32, 64, 3, 2, 1, bias=False)
+        self.imgblk8 = nn.Conv2d(64, 128, 3, 2, 1, bias=False)
+        self.imgblk4 = nn.Conv2d(128, 256, 3, 2, 1, bias=False)
 
-        self.encode32 = GBlock(32, 96, 5, 3)
-        self.encode16 = GBlock(64, 128, 5, 3)
-        self.encode8 = GBlock(128, 384, 5, 3)
-        self.encode4 = GBlock(256, 768, 7, 3)
+        self.encode32 = XBlock(32, 96, 7, 5)
+        self.encode16 = XBlock(64, 192, 7, 5)
+        self.encode8 = XBlock(128, 384, 7, 5)
+        self.encode4 = XBlock(256, 768, 7, 5)
 
         self.down16 = nn.Conv2d(32, 64, 3, 2, 1, bias=False)
         self.down8 = nn.Conv2d(64, 128, 3, 2, 1, bias=False)
         self.down4 = nn.Conv2d(128, 256, 3, 2, 1, bias=False)
 
         self.decode4 = nn.Sequential(
-            GBlock(256, 768, 7, 3),
+            XBlock(256, 768, 7, 5),
             nn.Upsample(scale_factor=2, mode='bilinear'),
             nn.Conv2d(256, 128, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.decode8 = nn.Sequential(
-            GBlock(128, 256, 5, 3),
+            XBlock(128, 256, 7, 5),
             nn.Upsample(scale_factor=2, mode='bilinear'),
             nn.Conv2d(128, 64, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.decode16 = nn.Sequential(
-            GBlock(64, 128, 5, 3),
+            XBlock(64, 196, 7, 5),
             nn.Upsample(scale_factor=2, mode='bilinear'),
             nn.Conv2d(64, 32, 3, 1, 1, bias=False),
-            nn.ReLU6()
+            nn.ReLU(inplace=True)
         )
         self.decode32 = nn.Sequential(
-            GBlock(32, 96, 5, 3),
+            XBlock(32, 96, 7, 5),
             nn.Conv2d(32, 3, 3, 1, 1),
             nn.Sigmoid()
         )
@@ -115,7 +128,7 @@ class Generator(nn.Module):
     def forward(self, x, depth, clip):
 
         x = torch.concat([x, depth], dim=1)
-        
+
         feat4 = self.clipblk4(clip)
         feat8 = self.clipblk8(feat4)
         feat16 = self.clipblk16(feat8)
--- train.py
+++ train.py
@@ -14,7 +14,7 @@ from pytorch_msssim import MS_SSIM
 from PIL import Image
 
 import mobileclip
-from GFLIP import Generator
+from GFLIP import Generator, Genloss
 from datasets import ImageSet
 from processors import InversePipe
 
@@ -60,34 +60,53 @@ if stats:
 
 print("Params:", params)
 
+genloss = Genloss().to(device)
+
 optimizer = optim.NAdam(gen.parameters(), lr=0.0001, betas=(0.0, 0.9))
 scaler = torch.cuda.amp.GradScaler()
 check = nn.HuberLoss()
+
+l_optimizer = optim.NAdam(genloss.parameters(), lr=0.0002, betas=(0.0, 0.9))
+l_scaler = torch.cuda.amp.GradScaler()
+l_check = nn.BCEWithLogitsLoss()
+
 ssim = MS_SSIM(1.0)
 depth = InversePipe('depth')
+dblur = T.GaussianBlur((9, 7), (10, 20))
 
 test_noise = torch.zeros(1, 3, 256, 256).to(device)
 test_image = preprocess(Image.open("./imgs/dogcat.jpg").convert('RGB')).cuda().unsqueeze(0)
 test_embeds = clip.encode_image(test_image, patch=True)
 test_depth = depth(test_image)
+test_blurd = dblur(test_depth)
 
 blank = torch.zeros(batchsize//2, 3, 256, 256).to(device)
+ones = torch.ones(batchsize, device=device)
+zeros = torch.zeros(batchsize, device=device)
 
 def train(inp, dep, out):
     optimizer.zero_grad()
     with torch.cuda.amp.autocast():
         out_embeds = clip.encode_image(out, patch=True)
-        guess = gen(inp, dep, out_embeds)
+
+        indep = dblur(dep)
+
+        for _ in range(batchsize//2):
+            indep[torch.randint(0, batchsize, (1,)).item()] = torch.zeros(1, 256, 256).to(device)
+
+        guess = gen(inp, indep, out_embeds)
         
         guess_embeds = clip.encode_image(guess, patch=True)
         clipsim = torch.cosine_similarity(guess_embeds, out_embeds).mean()
+        guess_depth = depth(guess)
 
         imgloss = check(guess, out)
         cliploss = (1 - clipsim)
         ssimloss = (1 - ssim(guess, out))
-        depthloss = check(depth(guess), dep)
+        depthloss = check(guess_depth, dep)
+        dloss = F.sigmoid(genloss(guess, guess_depth)).mean()
 
-        loss = cliploss * 0.1 + depthloss + imgloss
+        loss = cliploss * 0.25 + depthloss + imgloss + dloss * 0.1
 
     scaler.scale(loss).backward()
     scaler.step(optimizer)
@@ -95,9 +114,23 @@ def train(inp, dep, out):
     if scaler.get_scale() < 64:
         scaler.update(16384.0)
 
+    l_optimizer.zero_grad()
+    with torch.cuda.amp.autocast():
+        bad = genloss(guess.detach(), guess_depth.detach())
+        good = genloss(out, dep)
+        l_loss = l_check(bad, ones) + l_check(good, zeros)
+
+    l_scaler.scale(l_loss).backward()
+    l_scaler.step(l_optimizer)
+    l_scaler.update()
+    if l_scaler.get_scale() < 64:
+        l_scaler.update(16384.0)
+
     if stats:
         wandb.log({
             'loss': loss,
+            'g_loss': dloss,
+            'd_loss': l_loss,
             'image_loss': imgloss,
             'ssim_loss': ssimloss,
             'depth_loss': depthloss,
@@ -120,10 +153,14 @@ for epoch in range(epochs):
         
         if i % 50 == 0:
             print(f"[Epoch {epoch}/{epochs}] [Batch {i}/{len(dataloader)}] [loss: {loss.item()}] [CLIP: {clipsim.item()}]")
-            img = gen(test_noise, test_depth, test_embeds)
-            images = [test_image, img]
-            for x in range(7):
-                img = gen(img, test_depth, test_embeds)
+            img = test_noise.clone()
+            images = [
+                test_image,
+                gen(img, torch.zeros(1, 1, 256, 256).to(device), test_embeds),
+                gen(img, test_depth, test_embeds)
+            ]
+            for x in range(6):
+                img = gen(img, test_blurd, test_embeds)
                 images.append(img)
             T.ToPILImage()(make_grid(torch.concat(images), 3)).save(f"./results/{epoch}-{i}.png")
             torch.save(gen, f"./models/gen_{epoch}.pth")
