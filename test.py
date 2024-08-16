fixed masking

test.py
@@ -1,13 +1,13 @@
 import time
 import torch
+import torch.nn as nn
 import numpy as np
 from torchvision.transforms import v2 as T
 from torchvision.utils import make_grid
 from PIL import Image
-from pyperlin import FractalPerlin2D
 
-import mobileclip
 from GFLIP import Generator
+from processors import DepthPipe, getCLIP
 
 torch.backends.cudnn.benchmark = True
 
@@ -18,17 +18,6 @@ loadpt = 0
 mult = 1
 dtype = torch.float16
 
-clip, _, preprocess = mobileclip.create_model_and_transforms(f'mobileclip_s0', pretrained=f'./models/mobileclip_s0.pt')
-clip = clip.to(device, dtype)
-
-for p in clip.image_encoder.parameters():
-    p.requires_grad = False
-clip.image_encoder.eval()
-
-for p in clip.text_encoder.parameters():
-    p.requires_grad = False
-clip.text_encoder.eval()
-
 gen = Generator().to(device, dtype)
 
 gen.load_state_dict(torch.load(f'./models/gen_{loadpt}.pth').state_dict())
@@ -37,24 +26,68 @@ params = np.sum([p.numel() for p in gen.parameters()]).item()/10**6
 
 print("Params:", params)
 
-scale = torch.nn.Upsample(scale_factor=mult, mode='bilinear')
-test_noise = torch.zeros(1, 3, int(256 * mult), int(256 * mult)).to(device, dtype)
-test_image = preprocess(Image.open("./imgs/boujee.png").convert('RGB')).unsqueeze(0).to(device, dtype)
-test_embeds = clip.encode_image(test_image, patch=True)
-test_image = scale(test_image)
-test_embeds = scale(test_embeds)
+depth = DepthPipe(518)
+clip, preprocess = getCLIP()
+clip.to(device, dtype)
 
-images = [test_image]
-img = test_noise
+def getmask(b, c, m, p):
+    s = 256 // (2 ** p)
+    return nn.Upsample((256, 256))(torch.repeat_interleave((torch.rand(b, 1, s, s) < m).float(), c, 1).round()).to(device, dtype)
+
+scale = torch.nn.Upsample(scale_factor=mult, mode='bilinear')
+frameT = preprocess(Image.open("./imgs/modern.png").convert('RGB')).unsqueeze(0).to(device, dtype)
+embedT = clip.encode_image(frameT, patch=True)
+depthT = scale(depth(frameT))
+frameT = scale(frameT)
+embedT = scale(embedT)
+
+tests = []
+
+istack = torch.concat([
+    depthT,
+    frameT,
+    depthT,
+    frameT
+], dim=1)
+
+mask = torch.concat([
+    getmask(1, 1, 0.1, 0),
+    getmask(1, 3, 0.1, 0),
+    getmask(1, 1, 0.1, 0),
+    getmask(1, 3, 0.0, 0),
+], dim=1)
+imask = (1 - mask)
 
 with torch.inference_mode():
     for x in range(8):
+            
         t = time.time()
-        img = gen(img, test_embeds)
+        ostack = gen(istack, mask, embedT, embedT) * imask + istack * mask
         print('%.4fms -> ' % ((time.time()-t) * 1000), end='')
-        images.append(img)
-        guess_embeds = clip.encode_image(img, patch=True)
-        clipsim = torch.cosine_similarity(guess_embeds, test_embeds).mean()
+
+        tests.append(torch.repeat_interleave(ostack[:, :1], 3, 1))
+        tests.append(ostack[:, 1:4])
+        tests.append(torch.repeat_interleave(ostack[:, 4:5], 3, 1))
+        tests.append(ostack[:, -3:])
+
+        embedG = clip.encode_image(ostack[:, -3:], patch=True)
+        clipsim = torch.cosine_similarity(embedG, embedT).mean()
         print('%.4f' % clipsim.item())
 
-T.ToPILImage()(make_grid(torch.concat(images), 3)).save(f"./out.png")
+        istack = torch.concat([
+            depthT,
+            frameT,
+            # ostack[:, 1:4],
+            depthT,
+            ostack[:, -3:]
+        ], dim=1)
+        
+        mask = torch.concat([
+            getmask(1, 1, 0.15, 0),
+            getmask(1, 3, 0.15, 0),
+            getmask(1, 1, 0.15, 0),
+            getmask(1, 3, 0.05, 0),
+        ], dim=1)
+        imask = (1 - mask)
+
+T.ToPILImage()(make_grid(torch.concat(tests), 4)).save(f"./out.png")

train.py
@@ -62,7 +62,7 @@ depthT = depth(frameT)
 
 def getmask(b, c, m, p):
     s = 256 // (2 ** p)
-    return nn.Upsample((256, 256))(torch.repeat_interleave(torch.rand((b, 1, s, s)) * (0.5 + m * 0.5), c, 1).round()).to(device)
+    return nn.Upsample((256, 256))(torch.repeat_interleave((torch.rand(b, 1, s, s) < m).float(), c, 1).round()).to(device)
 
 t = time.time()
 